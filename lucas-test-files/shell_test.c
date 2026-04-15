e9230ee417cfba0b847c26feba9f89ec79834e46
lucas <zaleslucas3@gmail.com>
2026-04-15 18:56:41 +1000

    lucas test

---
+++ lucas-test-files/shell_test.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	pid_t pid;
+	int status;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("($) ");
+
+		if (getline(&line, &len, stdin) == -1)
+		{
+			free(line);
+			exit(0);
+		}
+
+		for (int i = 0; line[i] != '\0'; i++)
+		{
+			if (line[i] == '\n')
+			{
+				line[i] = '\0';
+				break;
+			}
+		}
+
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("Error");
+			continue;
+		}
+
+		if (pid == 0)
+		{
+			char *argv[] = {line, NULL};
+
+			if (execve(line, argv, NULL) == -1)
+			{
+				perror("./hsh");
+			}
+			exit(1);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+
+	free(line);
+	return (0);
+}
