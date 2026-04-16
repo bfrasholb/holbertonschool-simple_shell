d659e5fdac63efb009524935bff15c1a7469fff9
Sebastion Price <sebastionprice@gmail.com>
2026-04-16 14:40:15 +1000

    Fix mem leaks for shell

--- simple_shell.c
+++ simple_shell.c
@@ -15,7 +15,6 @@ int main(void)
 	{
 		RUN = shell(env, line);
 	}
-	free(line);
 	return (0);
 }
 
@@ -35,12 +34,18 @@ int shell(char **env, char *line)
 	struct stat st;
 	pid_t fork_id;
 
-	prompt("sybau");
+	prompt("shell$ ");
 	read = getline(&line, &len, stdin);
 	if (read == 1)
+	{
+		free(line);
 		return (0);
+	}
 	if (read == -1)
+	{
+		free(line);
 		return (1);
+	}
 	if (line[read - 1] == '\n')
 		line[read - 1] = '\0';
 	args = str_to_arr(line);
@@ -54,10 +59,17 @@ int shell(char **env, char *line)
 	if (strcmp(args[0], "env") == 0)
 	{
 		print_env(env);
+		free(line);
+		free_string_array(args, arglen);
 		return (0);
 	}
 	if (stat(args[0], &st) && search_path(&args[0], 1) == 1)
-		return (!(printf("command not found: %s\n", args[0])));
+	{
+		printf("command not found: %s\n", args[0]);
+		free(line);
+		free_string_array(args, arglen);
+		return (0);
+	}
 	fork_id = fork();
 	if (fork_id == 0 && execve(args[0], args, env) == -1)
 	{
@@ -66,6 +78,7 @@ int shell(char **env, char *line)
 	}
 	else
 		wait(NULL);
+	free(line);
 	free_string_array(args, arglen);
 	return (0);
 }
