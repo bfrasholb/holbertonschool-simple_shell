e93fe744a4aa0e8ac411dc4364fdc0362f785442
Sebastion Price <sebastionprice@gmail.com>
2026-04-21 12:12:29 +1000

    Revert "Fix exit status code"
    This reverts commit ae9d2d3183c57539155413cb6c67ec2eac205aa3.

--- simple_shell.c
+++ simple_shell.c
@@ -81,7 +81,7 @@ int run_command(char **argv, char **args, int arglen, char **env)
 	if (strcmp(args[0], "exit") == 0)
 	{
 		free_string_array(args, arglen);
-		return (2);
+		return (1);
 	}
 	if (strcmp(args[0], "env") == 0)
 	{
