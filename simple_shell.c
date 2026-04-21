d053b0a117ae18e6248b1c36265eda975aa1f4af
lucas <zaleslucas3@gmail.com>
2026-04-21 12:29:42 +1000

    replaced last_status for 0 in the first loop

--- simple_shell.c
+++ simple_shell.c
@@ -12,15 +12,13 @@ int main(int argc, char **argv, char **env)
 {
 	char *line = NULL;
 	int RUN = 0;
-	int last_status;
 
 	(void)argc;
 	while (RUN != 1)
 	{
-		last_status = RUN;
 		RUN = shell(argv, env, line);
 	}
-	return (last_status);
+	return (0);
 }
 
 /**
